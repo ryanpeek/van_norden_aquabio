2096ee273157f0f0aaef9f7848f11ca342a1d671
Ryan Peek <rapeek@ucdavis.edu>
2021-11-14 01:46:20 -0800

    add gw

---
+++ code/stitch_groundwater_compensated.R
@@ -0,0 +1,47 @@
+# read in groundwater loggers
+
+
+# Libraries ---------------------------------------------------------------
+
+library(tidyverse)
+library(fs)
+library(lubridate)
+library(glue)
+library(here)
+library(purrr)
+
+
+# List Files --------------------------------------------------------------
+
+comp_files <- fs::dir_ls("~/Box Sync/Van_Norden/DATA_RAW/hydrology/groundwater/loggers_downloaded/", 
+           recurse = TRUE,
+           regexp = "[Compensated].csv")
+
+# main dir
+main_sites <- basename(fs::path_dir(comp_files))
+
+# Read in Files -----------------------------------------------------------
+
+df_all <- read_csv(comp_files, id = "filename", skip = 11) %>% 
+  # add datetime
+  mutate(datetime_mdy=mdy_hms(glue("{Date} {Time}")),
+         datetime_ymd=ymd_hms(glue("{Date} {Time}"))) %>% 
+  mutate(datetime = coalesce(datetime_mdy, datetime_ymd)) %>% 
+  select(-c(datetime_mdy, datetime_ymd, ms, Date, Time)) %>% 
+  # fix filename
+  mutate(station = basename(fs::path_dir(filename)))
+
+summary(df_all)
+
+table(df_all$station)
+
+
+# Plot --------------------------------------------------------------------
+
+ggplot() + 
+  geom_line(data=df_all, aes(x=datetime, y=TEMPERATURE))+
+  facet_wrap(.~station)
+
+ggplot() + 
+  geom_line(data=df_all, aes(x=datetime, y=LEVEL), color="cyan4")+
+  facet_wrap(.~station)
